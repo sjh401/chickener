586b2aa71e1412d8d578f0d1fd3891ced13e98e9
stephenharrity <sjh401@gmail.com>
2021-07-14 10:46:36 -0500

    trying to get chicken postition

--- src/Components/Chicken/Chicken.jsx
+++ src/Components/Chicken/Chicken.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { createRef, useRef, useEffect, useState } from 'react'
 import "../../App.css"
 
 
 export default function Chicken(props) {
+    const [ chickenPosition, setChickenPosition ] = useState({})
+    const { chickenT } = createRef("");
+
+    function chickenTFocus () {
+        setChickenPosition(chickenT.current.focus())
+    }
+
+    if(chickenT === undefined) return(
+        <img src="https://png.pngtree.com/png-vector/20200417/ourlarge/pngtree-cute-chicken-cartoon-illustration-png-image_2184671.jpg" 
+        alt="Chicken" 
+        className="chicken" 
+        style={{
+        gridRowStart: `${props.NS}`,
+        // girdRowEnd: "10",
+        gridColumnStart: `${props.EW}`,
+        // girdColumnEnd: "7",
+        maxHeight: "50px",
+        // girdArea: "9/1/10/2",
+        maxWidth: "50px",
+        margin: "0 0 -3px 0",
+        zIndex: "1"}}/>
+    )
+    console.log(chickenPosition)
+    console.log(chickenT.current.focus())
     return (
             <img src="https://png.pngtree.com/png-vector/20200417/ourlarge/pngtree-cute-chicken-cartoon-illustration-png-image_2184671.jpg" 
             alt="Chicken" 
@@ -16,7 +40,9 @@ export default function Chicken(props) {
             // girdArea: "9/1/10/2",
             maxWidth: "50px",
             margin: "0 0 -3px 0",
-            zIndex: "1"}}/>
+            zIndex: "1"}}
+            ref={chickenT}
+            onClick={chickenTFocus}/>
     )
 }
 
--- src/Components/GameGrid.jsx
+++ src/Components/GameGrid.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Chicken from './Chicken/Chicken'
 
 
@@ -12,6 +12,9 @@ export default function GameGrid() {
     const [ grid, setGrid ] = useState([])
     const [ clickNS, setClickNS ] = useState(9)
     const [ clickEW, setClickEW ] = useState(6)
+    const [ coords, setCoords ] = useState({})
+    const [ chickenPos, setChickenPos ] = useState({})
+
 
     useEffect(() => {
         const getGrid = async () => {
@@ -25,42 +28,55 @@ export default function GameGrid() {
         return <div>Loading...</div>
     }
 
-    function up(click) {
+
+    function up(click , e) {
         if(clickNS < 1) return setClickNS(1)
         setClickNS((prevClick)=> prevClick - 1)
-        console.log(clickNS)
+        // setChickenPos({x: e?.nativeEvent.offsetX, y: e?.nativeEvent.offsetY})
+        // setChickenPos({x: offsetX, y: offsetY})
+        // console.log(clickNS)
     }
     function left(click) {
         if(clickEW < 3) return setClickEW(2)
         setClickEW((prevClick)=> prevClick - 1)
-        console.log(clickEW)
+        // console.log(clickEW)
     }
     function right(click) {
         if(clickEW > 9) return setClickEW(10)
         setClickEW((prevClick)=> prevClick + 1)
-        console.log(clickEW)
+        // console.log(clickEW)
     }
     function down(click) {
         if(clickNS > 8) return setClickNS(9)
         setClickNS((prevClick)=> prevClick + 1)
-        console.log(clickNS)
+        // console.log(clickNS)
     }
 
     const gridFilter = (order, block) => {
         if(block.fields?.order === order) { return <img key={block.id} src={block.fields.image} alt={block.fields.name} className="image-grid"/>} 
     }
 
+    function chickenTrack(e, chicken) {
+        let idX = chicken + "X";
+        let idY = chicken + "Y";
+    }
+    function update(e) {
+        e.preventDefault()
+        setCoords({x: e.nativeEvent.offsetX, y:e.nativeEvent.offsetY});
+    }
+    console.log(coords)
+    console.log(chickenPos)
     return (
-            <div className="game-board">
+            <div className="game-board" onClick={update}>
                 <Chicken NS={clickNS} EW={clickEW}/>
                 <div className="left-board"></div>
-                <div className="center-board">
-                    {grid.map((block) => gridFilter(1, block))}
+                <div className="center-board" >
+                    {/* {grid.map((block) => gridFilter(1, block))}
                     {grid.map((block) => gridFilter(2, block))}
-                    {grid.map((block) => gridFilter(3, block))}
+                    {grid.map((block) => gridFilter(3, block))} */}
                 </div>
                 <div className="right-board">
-                    <button onClick={up}>up</button>
+                    <button onClick={up} >up</button>
                     <br />
                     <button onClick={left}>left</button><button onClick={right}>right</button>
                     <br />
